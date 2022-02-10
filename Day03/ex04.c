b40667c0056eda555e90802d22012fd1b4890eb7
sheeps24 <zzztreez2@gmail.com>
2022-02-10 15:51:44 +0900

    Day03

---
+++ Day03/ex04.c
@@ -0,0 +1,21 @@
+
+
+
+#include <stdio.h>
+
+int main(void){
+
+    double a = 12.34;
+    int b;
+    b = (int) a;
+
+    printf(" a : %f \n", a);
+    printf(" b : %f \n", b);
+
+    printf("%d\n", (short) 3.1415);
+    printf("%d\n", (int) 3.1415);
+    printf("%f\n", (double) 10);
+    printf("%f\n", (float) 10);
+
+
+}
---
+++ Day04/ex01.c
@@ -0,0 +1,21 @@
+
+
+
+
+#include <stdio.h>
+
+int main(void) {
+    
+    int age;
+    char name[10];
+
+    printf("나이와 이름을 입력 : ");
+    // 입력 함수
+
+    scanf("%d %s", & age,name);
+    printf("나이 : %d\n", age);
+    printf("이름 : %s\n", name);
+
+
+    return 0;
+}
