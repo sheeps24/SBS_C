2bf5411c25c8394fe35ea0206f5231fb82d26fc8
sheeps24 <zzztreez2@gmail.com>
2022-02-15 15:12:12 +0900

    Day07

---
+++ Day07/ex06.c
@@ -0,0 +1,26 @@
+
+
+
+#include <stdio.h>
+// 짝수의 합계를 구하시오.
+int main(void){
+    int sum = 0;
+    for (int i = 1; i <= 20; i++){
+
+        // 홀수인 경우 넘어감.
+        if(i % 2 == 1 )
+            continue;
+
+        sum += i;
+
+        printf("%d",i);
+        if ( i == 20)
+            printf("=");
+        else
+            printf("+");
+
+    }
+
+    printf("%d",sum);
+    return 0;
+}
---
+++ Day07/ex07.c
@@ -0,0 +1,20 @@
+
+
+
+#include <stdio.h>
+// 짝수의 합계를 구하시오.
+int main(void){
+    int n = 1;
+
+    for ( ; ; ){
+        // 종료조건
+        printf(" n : %d\n", n);
+        if ( n == 30 )
+            break;
+        n++;
+
+    }
+
+
+    return 0;
+}
